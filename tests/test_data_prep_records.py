e2017deb5043a9b27d56d2147c04eb7807eb6d3f
David Rose <david010@gmail.com>
2023-10-02 14:33:13 -0400

    fix tests

--- tests/test_data_prep_records.py
+++ tests/test_data_prep_records.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 import pandas as pd
 import pytest
-from pathlib import Path
-from your_module import load_data, preprocess_data, get_top_activity, save_data
+from chatlas.data_prep.records import get_top_activity, load_data, preprocess_data, save_data
+
 
 # Update this to your actual path relative to the test file
 DEFAULT_RECORDS_PATH = Path("./data/sample/location_history/Records.json")
@@ -10,19 +13,17 @@ DEFAULT_RECORDS_PATH = Path("./data/sample/location_history/Records.json")
 @pytest.fixture(scope="function")
 def sample_data():
     """Load sample data from JSON file."""
-    return load_data(DEFAULT_RECORDS_PATH)
+    return load_data(DEFAULT_RECORDS_PATH, 1_000)
 
 
 def test_load_data(sample_data):
     """Test the load_data function."""
-    assert isinstance(sample_data, dict), "Loaded data should be a dictionary."
+    assert isinstance(sample_data, pd.DataFrame), "Loaded data should be a DataFrame."
 
 
 def test_preprocess_data(sample_data):
     """Test the preprocess_data function."""
-    df = pd.json_normalize(sample_data["locations"])
-    df["timestamp"] = pd.to_datetime(df["timestamp"])
-    preprocessed_df = preprocess_data(df)
+    preprocessed_df = preprocess_data(sample_data)
 
     assert "top_activity" in preprocessed_df.columns
     assert "confidence" in preprocessed_df.columns
@@ -30,20 +31,18 @@ def test_preprocess_data(sample_data):
 
 def test_get_top_activity(sample_data):
     """Test the get_top_activity function."""
-    activities = sample_data["locations"][0]["activity"]
-    result = get_top_activity(activities)
-    assert isinstance(result, dict), "Top activity should be a dictionary."
-    assert "type" in result
-    assert "confidence" in result
+    top_activities = sample_data["activity"].apply(get_top_activity)
+    percent_populated = top_activities.isna().sum() / len(sample_data)
+    assert isinstance(top_activities, pd.Series), "Function should return a Series."
+    assert percent_populated < 0.9, "More than 90% of the activities appear to be missing."
 
 
-def test_save_data(sample_data):
+def test_save_data(tmp_path):
     """Test the save_data function."""
-    df = pd.json_normalize(sample_data["locations"])
-    df["timestamp"] = pd.to_datetime(df["timestamp"])
-    preprocessed_df = preprocess_data(df)
+    df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+    output_file = tmp_path / "output.pkl"
+    save_data(df, output_file)
+    assert output_file.exists()
 
-    with TemporaryDirectory() as tmpdirname:
-        temp_path = Path(tmpdirname) / "temp.pkl"
-        save_data(preprocessed_df, temp_path)
-        assert temp_path.exists(), "Pickle file was not saved."
+    loaded_df = pd.read_pickle(output_file)
+    pd.testing.assert_frame_equal(df, loaded_df)
