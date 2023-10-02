9be69548241e9d380902d8244e4395417dc11b72
David Rose <david010@gmail.com>
2023-10-02 13:07:17 -0400

    refactor

--- chatlas/data_prep/records.py
+++ chatlas/data_prep/records.py
@@ -13,18 +13,28 @@ DEFAULT_RECORDS_PATH = Path(".../data/sample/location_history/Records.json")
 DEFAULT_OUTPUT_PATH = Path(".../data/sample/processed/records.pkl")
 
 
-def load_data(file_path: Path) -> Dict:
-    """Load data from a JSON file."""
+def load_data(file_path: Path) -> pd.DataFrame:
+    """
+    Load data from a JSON file and normalize it to a DataFrame.
+    """
     logging.info(f"Loading data from {file_path}...")
     with file_path.open("r") as f:
-        data = json.load(f)
-    logging.info("Data loaded successfully.")
-    return data
+        raw_data = json.load(f)
+
+    df = pd.json_normalize(raw_data["locations"])
+    logging.info("Data loaded and normalized successfully.")
+    return df
 
 
 def preprocess_data(df: pd.DataFrame, sample_n: Optional[int] = None) -> pd.DataFrame:
-    """Preprocess DataFrame."""
+    """
+    Preprocess DataFrame.
+    """
     logging.info("Preprocessing data...")
+
+    # Convert timestamp to datetime format
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+
     if sample_n:
         df = df.sample(sample_n)
         logging.info(f"Sampled {sample_n} rows from the DataFrame.")
@@ -39,12 +49,15 @@ def preprocess_data(df: pd.DataFrame, sample_n: Optional[int] = None) -> pd.Data
 
     # Convert all string data to lowercase
     df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
     logging.info("Preprocessing completed.")
     return df
 
 
 def get_top_activity(activities: Union[List[Dict], float]) -> Optional[Dict]:
-    """Extract the activity with the highest confidence."""
+    """
+    Extract the activity with the highest confidence.
+    """
     if isinstance(activities, list):
         if not activities:
             return None
@@ -55,7 +68,9 @@ def get_top_activity(activities: Union[List[Dict], float]) -> Optional[Dict]:
 
 
 def save_data(df: pd.DataFrame, output_file: Path) -> None:
-    """Save DataFrame to a pickle file."""
+    """
+    Save DataFrame to a pickle file.
+    """
     logging.info("Saving processed data...")
     output_dir = output_file.parent
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -64,13 +79,11 @@ def save_data(df: pd.DataFrame, output_file: Path) -> None:
 
 
 if __name__ == "__main__":
-    # Load raw data
-    raw_data = load_data(DEFAULT_RECORDS_PATH)
+    # Load raw data and convert to DataFrame
+    df = load_data(DEFAULT_RECORDS_PATH)
 
-    # Convert JSON to DataFrame and preprocess
-    df_loc = pd.json_normalize(raw_data["locations"])
-    df_loc["timestamp"] = pd.to_datetime(df_loc["timestamp"])
-    df = preprocess_data(df_loc, sample_n=10_000)
+    # Preprocess DataFrame
+    df_processed = preprocess_data(df, sample_n=10_000)
 
     # Save the preprocessed data
-    save_data(df, DEFAULT_OUTPUT_PATH)
+    save_data(df_processed, DEFAULT_OUTPUT_PATH)
